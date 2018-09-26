7d29049b290c8cd5520f9996e083b41a60e8b5db
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2018-09-27 00:28:59 +0300

    finish lab1

---
+++ lab1/logs/compile/my_open_ru.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main ( argc, argv )
+int argc;
+char *argv [];
+{
+	char str[20];
+	scanf("%s", str);
+	if (str != NULL) {
+		if (fopen(str,"r") == NULL)
+			printf("%s: неудача при попытке открыть файл %s",argv[0],str);
+		else
+			printf("%s: файл %s открыт",argv[0],str);
+	} else {
+		printf("открываемый файл не указан");
+	}
+}
