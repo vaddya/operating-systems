55e05b9195c3d221280505a6e54118f5c75deab4
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2018-11-17 16:37:52 +0300

    lab2 updates

--- lab2/logs/4_dir/generate.sh
+++ lab2/logs/4_dir/generate.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 mkdir fold
+SIZE=0
 for I in $(seq 0 $1)
 do
 	echo $I > ./fold/$I
-	S=$(du -h ./fold/ | cut -f 1)
-	echo "$I: Folder size: $S"
+	S=$(ls -l | grep "fold" | cut -f5 -d" ")
+	if [ $SIZE -ne $S ]; then
+		echo "$I: Folder size: $S"
+	fi
+	SIZE=$S
 done
-rm -rf fold
+rm -r fold
